0fed5057f72a96d9818e457ec0e05cb9392637d4
Pavel <meeney017@gmail.com>
2021-03-11 19:25:00 +0500

    add new api entities

--- src/main/java/com/rincentral/test/models/BodyCharacteristics.java
+++ src/main/java/com/rincentral/test/models/BodyCharacteristics.java
@@ -1,5 +1,23 @@
 package com.rincentral.test.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Getter
+@EqualsAndHashCode
 public class BodyCharacteristics {
-    
+    @JsonProperty("body_length")
+    private final Integer bodyLength;
+
+    @JsonProperty("body_width")
+    private final Integer bodyWidth;
+
+    @JsonProperty("body_height")
+    private final Integer bodyHeight;
+
+    @JsonProperty("body_style")
+    private final String bodyStyle;
 }
--- src/main/java/com/rincentral/test/models/EngineCharacteristics.java
+++ src/main/java/com/rincentral/test/models/EngineCharacteristics.java
@@ -1,5 +1,25 @@
 package com.rincentral.test.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.rincentral.test.models.external.enums.EngineType;
+import com.rincentral.test.models.external.enums.FuelType;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Getter
+@EqualsAndHashCode
 public class EngineCharacteristics {
-    
+    @JsonProperty("engine_type")
+    private final FuelType fuelType;
+
+    @JsonProperty("engine_cylinders")
+    private final EngineType engineType;
+
+    @JsonProperty("engine_displacement")
+    private final Integer engineDisplacement;
+
+    @JsonProperty("engine_horsepower")
+    private final Integer hp;
 }
